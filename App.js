76c5cda50bd10507fadace209031a005cd5f2579
dgnaw <dogianam09072005@gmail.com>
2026-01-12 12:54:01 +0700

    Initializing commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Xin chào Đỗ Gia Nam - 23810310245!</Text>
       <StatusBar style="auto" />
     </View>
   );
